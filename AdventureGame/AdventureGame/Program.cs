1ddaee5175f11e273f9900a040cf3e8d28b115da
harryjmvowles <29339644@students.lincoln.ac.uk>
2025-02-18 13:52:12 +0000

    Started World building in Program FIle. Created a Player Class with some default Values. Created empty Room Class for room creation (needs using).

--- AdventureGame/AdventureGame/Program.cs
+++ AdventureGame/AdventureGame/Program.cs
@@ -8,11 +8,37 @@ namespace AdventureGame
 {
     class Program
     {
-        
+        public static Player currentPlayer = new Player();
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello, World!");
+            start();
+        }
+
+        static void start()
+        {
+            Console.WriteLine("|| THE DUNGEON ||");
+            Console.WriteLine("Hello Adventurer, Brave enough to enter The Dungeon i see...");
+            Console.WriteLine("What is your name?: ");
+            currentPlayer.name = Console.ReadLine();
+
+            while (currentPlayer.name == "")
+            {
+                Console.WriteLine("You must enter a name!");
+                currentPlayer.name = Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine($"Well GoodLuck {currentPlayer.name}! Take these.... You will need them.");
+            Console.WriteLine(" [+2 Potions] " +
+                "Press Any Key to continue....");
+            currentPlayer.potions += 2;
+            Console.ReadKey();
+            Console.Clear();
+        
+            Console.WriteLine("The mysterious man at the entrance of The Dungeon suddenly disappeared, leaving you to face the tall, rusted metal door... The Enterance. ");
+            Console.WriteLine("You slowly push the door open, creaking and grating of the metal echoing through the many chambers as you enter. The door slams behind you,");
+            Console.WriteLine("a sort of magic sealing you within. You are now trapped in The Dungeon.... How will you escape. ");
+
         }
     }
 }
