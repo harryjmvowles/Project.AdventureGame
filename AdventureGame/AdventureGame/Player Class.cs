6b8913cb2a7df399da29acb6fd259c3b02300f28
harryjmvowles <29339644@students.lincoln.ac.uk>
2025-02-18 13:52:12 +0000

    Started World building in Program FIle. Created a Player Class with some default Values. Created empty Room Class for room creation (needs using).

---
+++ AdventureGame/AdventureGame/Player Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Player
+    {
+        public string name;
+        public int health = 100;
+        public int attack = 10;
+        public int defense = 5;
+        public int gold = 0;
+        public int potions = 0;
+        public int keys = 0;
+        public int level = 1;
+        public int experience = 0;
+        public int experienceToNextLevel = 100;
+    }
+}
--- AdventureGame/AdventureGame/Program.cs
+++ AdventureGame/AdventureGame/Program.cs
@@ -8,11 +8,40 @@ namespace AdventureGame
 {
     class Program
     {
-        
+        public static Player currentPlayer = new Player();
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello, World!");
+            start();
+        }
+
+        static void start()
+        {
+            Console.WriteLine("|| THE DUNGEON ||");
+            Console.WriteLine("Hello Adventurer, Brave enough to enter The Dungeon i see...");
+            Console.WriteLine("What is your name?: ");
+            currentPlayer.name = Console.ReadLine();
+
+            while (currentPlayer.name == "")
+            {
+                Console.WriteLine("You must enter a name!");
+                currentPlayer.name = Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine($"Well GoodLuck {currentPlayer.name}! Take these.... You will need them.");
+            Console.WriteLine(" [+2 Potions] " +
+                "Press Any Key to continue....");
+            currentPlayer.potions += 2;
+            Console.ReadKey();
+            Console.Clear();
+        
+            Console.WriteLine("The mysterious man at the entrance of The Dungeon suddenly disappeared, leaving you to face the tall, rusted metal door.");
+            Console.WriteLine("You slowly push the door open, creaking and grating of the metal echoing through the many chambers as you enter.");
+            Console.WriteLine("The door slams behind you, a sort of magic sealing you within.");
+            Console.WriteLine(" You are now trapped in The Dungeon.... How will you escape?");
+
+
+
         }
     }
 }
